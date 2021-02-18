9ae63087bc9a12094901495cad110d54b02c01a5
aistestonciute <72549610+aistestonciute@users.noreply.github.com>
2021-02-18 20:37:41 +0200

    Update main_array.cpp

--- main_array.cpp
+++ main_array.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <stdlib.h>
 #include <time.h> 
+#include <vector>
+#include <numeric>
+
+#define outputMedian  "median"
+#define outputAverage  "mean"
 
 using namespace std;
 
+// Struktura saugo vieno studento informacija
 struct Student
 {
     string name;
@@ -16,9 +21,8 @@ struct Student
     double final;
 
 };
-Student S[10000];
 
-
+//Tikrina ar ivesta reiksme atititnka reikalavimus (yra skaicius, pazymys priklauso [0,10] intervalui)
 bool isCorrectNumber(string temp, int maxGrade, int minGrade)
 {
     bool isCorrectValue = true;
@@ -33,22 +37,23 @@ bool isCorrectNumber(string temp, int maxGrade, int minGrade)
         }
 
     }
-     if (isCorrectValue == true && stoi(temp) < 1  && maxGrade == 0 && minGrade == 0)
-        {
-                 isCorrectValue = false;
-                 cout << "Error! Number must be greater than 0! ";
-        }
+    if (isCorrectValue == true && stoi(temp) < 1 && maxGrade == 0 && minGrade == 0)
+    {
+        isCorrectValue = false;
+        cout << "Error! Number must be greater than 0! ";
+    }
 
 
-     else if (isCorrectValue == true && (stoi(temp) < minGrade || stoi(temp) > maxGrade) && maxGrade != 0)
-         {
-                isCorrectValue = false;
-                cout << "Error! Number must be in range 0 to 10! ";
-         }
+    else if (isCorrectValue == true && (stoi(temp) < minGrade || stoi(temp) > maxGrade) && maxGrade != 0)
+    {
+        isCorrectValue = false;
+        cout << "Error! Number must be in range 0 to 10! ";
+    }
 
     return isCorrectValue;
 }
 
+//Skaiciaus ivedimo funkcija (si funkcija naudojama ir zinant n ir ne)
 int CorrectNumber(string input, int maxGrade, int minGrade, bool stop)
 {
     int x;
@@ -60,15 +65,15 @@ int CorrectNumber(string input, int maxGrade, int minGrade, bool stop)
         cin >> temp;
         if (temp == "stop" && stop == true) break;
 
-        
+
     } while (!isCorrectNumber(temp, maxGrade, minGrade));
 
-    if(temp != "stop")x = stoi(temp);
+    if (temp != "stop")x = stoi(temp);
     else x = -1;
     return x;
 }
 
-
+//Skaiciaus ivedimo funkcija (su maziau parametru)
 int CorrectNumber(string input)
 {
     int a = CorrectNumber(input, 0, 0, false);
@@ -76,7 +81,8 @@ int CorrectNumber(string input)
 }
 
 
-void Stop(string input, int maxGrade, int minGrade, int nd[], int &n)
+//Patikrina ar ivesta reiksme nera "stop" (naudojama pazymiu rasymui, kai nezinoma n)
+void Stop(string input, int maxGrade, int minGrade, int nd[], int& n)
 {
     bool stop = true;
     int temp;
@@ -84,7 +90,7 @@ void Stop(string input, int maxGrade, int minGrade, int nd[], int &n)
 
     while (stop)
     {
-        
+
         temp = CorrectNumber(input, maxGrade, minGrade, true);
         if (temp != -1)
         {
@@ -96,12 +102,11 @@ void Stop(string input, int maxGrade, int minGrade, int nd[], int &n)
             if (n == 0)cout << "Error! Enter at least one number! ";
             else stop = false;
         }
-        
+
     }
 }
 
-
-
+// Patikrinama ar ivesta varda/pavarde sudaro tik raides
 bool isCorrectString(string var)
 {
     bool isCorrect = true;
@@ -120,6 +125,7 @@ bool isCorrectString(string var)
     return isCorrect;
 }
 
+//Jei ivesta varda/pavarde sudaro tik raides, programa iraso ta kintamaji. Jei ne, prasoma ivesti is naujo.
 string CorrectString(int x, string input)
 {
     string var;
@@ -128,11 +134,11 @@ string CorrectString(int x, string input)
         cout << "Enter " << x + 1 << " student's" << input << " : ";
         cin >> var;
     } while (!isCorrectString(var));
-    
+
     return var;
 }
 
-
+//Skaiciuojama pazymiu suma, reikalinga vidurkiui.
 int Sum(int n, int nd[])
 {
     int sum = 0;
@@ -142,15 +148,15 @@ int Sum(int n, int nd[])
     }
     return sum;
 }
-
+//Galutinio vidurkio skaiciavimas
 double Average(int n, int nd[], int egz)
 
 {
-    double average = 0.4 * (Sum(n, nd)/n * 1.0) + 0.6 * egz;
+    double average = 0.4 * (Sum(n, nd) * 1.0 / n) + 0.6 * egz;
     return average;
 }
 
-
+//Pazymiu rikiavimas, reikalingas medianos radimui 
 void Sorting(int nd[], int n)
 {
     for (int i = 0; i < n - 1; i++)
@@ -160,8 +166,11 @@ void Sorting(int nd[], int n)
             if (nd[i] < nd[j]) swap(nd[i], nd[j]);
         }
     }
+
+
 }
 
+//Studentu pavardziu rikiavimas, reikalingas programos isvedimui i ekrana.
 void Sorting(Student S[], int s)
 {
     for (int i = 0; i < s - 1; i++)
@@ -173,14 +182,14 @@ void Sorting(Student S[], int s)
     }
 }
 
-
+//Ieskomas ilgiausias vardas/pavarde, reikalingas programos isvedimui i ekrana.
 int Max(Student S[], int s, bool isTrue)
 {
     int max = 0;
 
     for (int i = 0; i < s; i++)
     {
-        
+
         if (S[i].lastName.length() > max && isTrue)
         {
             max = S[i].lastName.length();
@@ -194,28 +203,30 @@ int Max(Student S[], int s, bool isTrue)
     return max;
 }
 
+//Medianos skaiciavimas
 double Median(int nd[], int n)
 {
     double median;
     Sorting(nd, n);
     if (n % 2 != 0) median = nd[n / 2];
-    else median = (nd[n / 2 - 1] + nd[n/ 2]) / 2.0;
+    else median = (nd[n / 2 - 1] + nd[n / 2]) / 2.0;
 
     return median;
 }
 
+//Isvedimo i ekrana funkcija
 void Print(Student S[], int s, string output)
 {
-    
+
     string line = "";
     Sorting(S, s);
     int maxLastName = Max(S, s, true);
     int maxName = Max(S, s, false);
-    cout << left << setw(maxLastName + 10) << "Last name" << setw(maxName + 10) << "Name" << "Final (" << output << ")" << endl;
-    
+    cout << endl << left << setw(maxLastName + 10) << "Last name" << setw(maxName + 10) << "Name" << "Final (" << output << ")" << endl;
+
     line.append(maxLastName + maxName + 40, '-');
     cout << line << endl;
-    
+
     for (int i = 0; i < s; i++)
     {
         cout << left << setw(maxLastName + 10) << S[i].lastName << setw(maxName + 10) << S[i].name << fixed << setprecision(2) << S[i].final << endl;
@@ -223,15 +234,16 @@ void Print(Student S[], int s, string output)
     cout << endl;
 }
 
+//Atsitiktinio pazymio generavimas
 int RandomGrade()
 {
     int grade;
-    grade = rand() % (10 + 1);
+    grade = 1 + rand() % ((10 + 1) - 1);
     return grade;
 }
 
-
-string YN ()
+//Tikrinama ar ivesta reiksme yra 'y' arba 'n'. Vykdoma tol, kol reiksme ivesta teisingai.
+string YN()
 {
     bool isCorrect = true;
     string yn;
@@ -240,7 +252,7 @@ string YN ()
     {
         cin >> yn;
         if (yn.length() == 1 && (tolower(yn[0]) == 'y' || tolower(yn[0]) == 'n')) isCorrect = true;
-        
+
         else
         {
             cout << "Error! Enter letter 'y' (yes) or 'n' (no): ";
@@ -252,89 +264,120 @@ string YN ()
     return yn;
 }
 
-
-int main()
+void MainFunction(int i, Student S[], char yn)
 {
-  
+    //Klaidu kodai, leidziantys vartotojui geriau suprasti, kur padare klaida.
     string inputName = " name";
     string inputLastName = " last name";
-    string input_s = "Enter the number of students: ";
     string input_n = "Enter the number of homework tasks: ";
     string input_nd = "Enter homework grades: ";
     string input_egz = "Enter exam grade: ";
-    string outputMedian = "median";
-    string outputAverage = "mean";
 
-    srand(time(0));
-
-
-    int maxGrade = 10, minGrade = 0;
-    
-    cout << "Do you want the final grade to be the mean (average)? (y/n) ";
-    char yn = (YN())[0];
-
-    char ynGrades;
-    int s = CorrectNumber(input_s);
-    
+    int maxGrade = 10, minGrade = 1;
     double median;
 
-    for (int i = 0; i < s; i++)
-    {
-        S[i].name = CorrectString(i, inputName);
-        S[i].name[0] = toupper(S[i].name[0]);
-        S[i].lastName = CorrectString(i, inputLastName);
-        S[i].lastName[0] = toupper(S[i].lastName[0]);
-        cout << "Do you want to enter the number of grades? (y/n) ";
-        ynGrades = (YN()[0]);
+    S[i].name = CorrectString(i, inputName);
+    S[i].lastName = CorrectString(i, inputLastName);
+    S[i].name[0] = toupper(S[i].name[0]);
+    S[i].lastName[0] = toupper(S[i].lastName[0]);
 
+    cout << "Do you want to enter the number of grades? (y/n) ";
+    char ynGrades = (YN()[0]);
+
+    if (ynGrades == 'y')
+    {
+        S[i].n = CorrectNumber(input_n);
+        cout << "Do you want to enter grades manually? (y/n) ";
+        ynGrades = (YN()[0]);
         if (ynGrades == 'y')
         {
-            S[i].n = CorrectNumber(input_n);
-            cout << "Do you want to enter grades manually? (y/n) ";
-            ynGrades = (YN()[0]);
 
-            if (ynGrades == 'y')
+            for (int j = 0; j < S[i].n; j++)
             {
-
-                for (int j = 0; j < S[i].n; j++)
-                {
-                    S[i].nd[j] = CorrectNumber(input_nd, maxGrade, minGrade, false);
-                }
-            }
-
-            else
-            {
-                for (int j = 0; j < S[i].n; j ++) S[i].nd[j] = RandomGrade();
-                cout << "Generated grades: ";
-                for (int j = 0; j < S[i].n - 1; j++) cout << S[i].nd[j] << ", ";
-                cout << S[i].nd[S[i].n - 1] << "." << endl;
-            }
-
-            cout << "Do you want to enter exam grade manually? (y/n) ";
-            ynGrades = (YN()[0]);
-
-            if (ynGrades == 'y') S[i].egz = CorrectNumber(input_egz, maxGrade, minGrade, false);
-            else
-            {
-                S[i].egz = RandomGrade();
-                cout << "Generated exam grade: " << S[i].egz << endl;
+                S[i].nd[j] = CorrectNumber(input_nd, maxGrade, minGrade, false);
             }
         }
-        else
-         {
-            int j = 0;
-            cout << "Enter grades (to end the cycle, enter 'stop'). " << endl;
-            Stop(input_nd, maxGrade, minGrade, S[i].nd, S[i].n);
-         }
 
-        if (yn == 'y') S[i].final = Average(S[i].n, S[i].nd, S[i].egz);
-        else S[i].final = Median(S[i].nd, S[i].n); 
+        else
+        {
+            for (int j = 0; j < S[i].n; j++) S[i].nd[j] = RandomGrade();
+            cout << "Generated grades: ";
+            for (int j = 0; j < S[i].n - 1; j++) cout << S[i].nd[j] << ", ";
+            cout << S[i].nd[S[i].n - 1] << "." << endl;
+        }
         
     }
 
+    else
+    {
+        cout << "Enter grades (to end the cycle, enter 'stop'). " << endl;
+        Stop(input_nd, maxGrade, minGrade, S[i].nd, S[i].n);
+    }
 
-    if(yn == 'y')Print(S, s, outputAverage);
-    else Print(S, s, outputMedian);
+    cout << "Do you want to enter exam grade manually? (y/n) ";
+    ynGrades = (YN()[0]);
+
+    if (ynGrades == 'y')  S[i].egz = CorrectNumber(input_egz, maxGrade, minGrade, false);
+    else
+    {
+        S[i].egz = RandomGrade();
+        cout << "Generated exam grade: " << S[i].egz << endl;
+    }
+    if (yn == 'y')  S[i].final = Average(S[i].n, S[i].nd, S[i].egz);
+    else  S[i].final = Median(S[i].nd, S[i].n);
+}
+
+void ManualInput(int s, Student S[], char yn)
+{
+    for (int i = 0; i < s; i++) MainFunction(i, S, yn);
+}
+
+void UnknownInput(int &s, Student S[], char yn)
+{
+    bool Continue = true;
+
+    while (Continue)
+    {
+        MainFunction(s, S, yn);
+        cout << "Do you want to add another student? (y/n) ";
+        char ynContinue = (YN()[0]);
+        if (ynContinue == 'n')
+        {
+            Continue = false;
+            break;
+        }
+        else  s++;
+        
+    }
+}
+
+int main()
+{
+
+    string input_s = "Enter the number of students: ";
+    int s = 0;
+    Student* S = new Student [10000];
+    srand(time(0));
+
+    cout << "Do you want to enter the number of students? (y/n) ";
+    char ynInput = (YN())[0];
+
+    cout << "Do you want the final grade to be mean (average)? (y/n) ";
+    char yn = (YN())[0];
+
+    if (ynInput == 'y')
+    {
+        s = CorrectNumber(input_s);
+        ManualInput(s, S, yn);
+    }
+    else
+    {
+        UnknownInput(s, S, yn);
+        s++;
+    }
+
+    if (yn == 'y')Print(S, s, outputAverage);
+    else Print(S, s , outputMedian);
 
 
     return 0;
