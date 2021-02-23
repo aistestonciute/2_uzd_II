6b9c372728f80b8fbf242840bbd98cd823cfe83a
aistestonciute <72549610+aistestonciute@users.noreply.github.com>
2021-02-23 19:19:21 +0200

    Add exception handling

--- main.cpp
+++ main.cpp
@@ -283,10 +283,10 @@ int main()
     int s = 0;
     vector <Student> Students;
     bool final;
-    
+
+    try {
         cout << "Do you want the final grade to be mean (average)? (y/n) ";
         char yn = (YN())[0];
-
         if (yn == 'y') final = true;
         else final = false;
 
@@ -304,12 +304,13 @@ int main()
                 ManualInput(s, Students, yn, final);
             }
             else UnknownInput(++s, Students, yn, final);
-            
+
         }
         else
         {
             ifstream in;
-            in.open("seip.txt");          
+            in.open("kursiokai.txt");
+            if (!in) throw 1;
             string line;
             getline(in, line);
 
@@ -320,29 +321,57 @@ int main()
                 int n = 0;
                 istringstream in(line);
                 in >> student.name >> student.lastName;
-                
+
                 while (in >> grade)
                 {
-                    student.nd.push_back(stoi(grade));
+                    if (grade.length() > 2) throw 3;
+                    if (!isdigit(grade[0]) || (grade.length() == 2 && !isdigit(grade[1]))) throw 4;
+                    if (stoi(grade) > 10 || stoi(grade) < 1) throw 5;
+                    else student.nd.push_back(stoi(grade));
                     n++;
                 }
                 n--;
                 student.n = n;
+                if (student.nd.size() == 0) throw 2;
                 student.nd.pop_back();
                 student.egz = stoi(grade);
                 if (final)  student.final = Average(student.n, student.nd, student.egz);
-                else  student.final = (Median(student.nd, student.n)) *0.4 + student.egz * 0.6;
+                else  student.final = (Median(student.nd, student.n)) * 0.4 + student.egz * 0.6;
                 Students.push_back(student);
                 s++;
             }
             in.close();
-
-
         }
 
-    if (final)Print(Students, s, outputAverage);
-    else Print(Students, s, outputMedian);
+    }
+    catch (int e)
+    {
+        cout << "Error! ";
+        switch (e) {
+        case 1:
+            cout << "File not opened." << endl;
+            break;
+        case 2:
+            cout << "Empty lines in file." << endl;
+            break;
+        case 3:
+            cout << "Grade must contain only one or two symbols (digits)." << endl;
+            break;
+        case 4:
+            cout << "Grade must contain only digits." << endl;
+            break;
+        case 5:
+            cout << "Grade must be in range 1 to 10." << endl;
+            break;
+        default:
+            cout << "System failure." << endl;
+            break;
+        }
+        exit(1);
+    }
+        if (final)Print(Students, s, outputAverage);
+        else Print(Students, s, outputMedian);
 
-    Students.clear();
-    return 0;
+        Students.clear();
+        return 0;    
 }
