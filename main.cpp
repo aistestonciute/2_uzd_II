e04955d8858188a584321dd20a7df82e4a27f473
aistestonciute <72549610+aistestonciute@users.noreply.github.com>
2021-02-24 22:18:01 +0200

    Fix RandomGrade()

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@
 #include <algorithm>
 #include <fstream>
 #include <sstream>
-#include <chrono>
-#include <random>
+#include <time.h>
+#include <cstdlib>
 #include "functions.hpp"
 
 #ifdef _WIN32
@@ -29,24 +29,20 @@ int main()
 
     try {
         cout << "Do you want the final grade to be mean (average)? (y/n) ";
-        char yn = (YN())[0];
-        if (yn == 'y') final = true;
+        if (Confirm()) final = true;
         else final = false;
 
         cout << "Do you want to enter data manually? (y/n) ";
-        yn = (YN())[0];
-
-        if (yn == 'y')
+        if (Confirm())
         {
             cout << "Do you want to enter the number of students? (y/n) ";
-            yn = (YN())[0];
 
-            if (yn == 'y')
+            if (Confirm())
             {
                 s = CorrectNumber(input_s);
-                ManualInput(s, Students, yn, final);
+                ManualInput(s, Students, final);
             }
-            else UnknownInput(++s, Students, yn, final);
+            else UnknownInput(++s, Students, final);
 
         }
         else
@@ -63,30 +59,24 @@ int main()
             while (getline(buffer, line))
             {
                 Student student;
-                string grade;
-                int n = 0;
                 stringstream in(line);
+                int grade;
                 in >> student.name >> student.lastName;
 
                 while (in >> grade)
                 {
-                    if (grade.length() > 2) throw 3;
-                    if (!isdigit(grade[0]) || (grade.length() == 2 && !isdigit(grade[1]))) throw 4;
-                    if (stoi(grade) > 10 || stoi(grade) < 1) throw 5;
-                    else student.nd.push_back(stoi(grade));
-                    n++;
+                    if (grade > 10 || grade < 1) throw 3;
+                    else student.nd.push_back(grade);
                 }
-                n--;
-                student.n = n;
                 if (student.nd.size() == 0) throw 2;
                 student.nd.pop_back();
-                student.egz = stoi(grade);
+                student.n = student.nd.size();
+                student.egz = grade;
                 if (final)  student.final = Average(student.n, student.nd, student.egz);
                 else  student.final = (Median(student.nd, student.n)) * 0.4 + student.egz * 0.6;
                 Students.push_back(student);
-                s++;
             }
-
+            s = Students.size();
         }
 
     }
@@ -101,12 +91,6 @@ int main()
             cout << "Empty lines in file." << endl;
             break;
         case 3:
-            cout << "Grade must contain only one or two symbols (digits)." << endl;
-            break;
-        case 4:
-            cout << "Grade must contain only digits." << endl;
-            break;
-        case 5:
             cout << "Grade must be in range 1 to 10." << endl;
             break;
         default:
