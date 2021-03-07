9909ca2b367c0213daad5dcc83bbf9fb41ddb7af
aistestonciute <72549610+aistestonciute@users.noreply.github.com>
2021-03-07 16:20:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,8 +24,6 @@ using namespace std;
 int main()
 {
     string input_s = "Enter the number of students: ";
-    string outputMedian = "median";
-    string outputAverage = "mean";
     long int s = 0;
     vector <Student> Students;
     bool final;
@@ -50,69 +48,26 @@ int main()
         }
         else
         {
-            cout << "Do you want to generate *.txt files? (y/n) "; 
-            if (Confirm())
-            {
-                cout << " > To use kursiokai1000.txt enter '1'." << endl;
-                cout << " > To use kursiokai10000.txt enter '2'." << endl;
-                cout << " > To use kursiokai100000.txt enter '3'." << endl;
-                cout << " > To use kursiokai1000000.txt enter '4'." << endl;
-                cout << " > To use kursiokai10000000.txt enter '5'." << endl;
-                cout << "Enter which file do you want to generate and use: ";
-                
-                int fileNumber = FileNumber();
-
-                if (fileNumber == 1)
-                {
+                    cout << endl << "> Using kursiokai1000.txt." << endl;
                     GenerateStudent(1000);
                     InputFiles(final, Students, "kursiokai1000.txt");
-                }
-                if (fileNumber == 2)
-                {
+                
+                    cout << endl << "> Using kursiokai10000.txt." << endl;
                     GenerateStudent(10000);
                     InputFiles(final, Students, "kursiokai10000.txt");
-                }
-                if (fileNumber == 3)
-                {
+                
+                    cout << endl << "> Using kursiokai100000.txt." << endl;
                     GenerateStudent(100000);
                     InputFiles(final, Students, "kursiokai100000.txt");
-                }
-                 if (fileNumber == 4)
-                {
+
+                    cout << endl << "> Using kursiokai1000000.txt." << endl;
                     GenerateStudent(1000000);
                     InputFiles(final, Students, "kursiokai1000000.txt");
-                }
-                 if (fileNumber == 5)
-                {
+
+                    cout << endl << "> Using kursiokai10000000.txt." << endl;
                     GenerateStudent(10000000);
-                    InputFiles(final, Students, "kursiokai10000000.txt");
-                }    
-            }
-            
-            else InputFiles(final, Students, "kursiokai.txt"); 
+                    InputFiles(final, Students, "kursiokai10000000.txt");          
         }
-    s = Students.size();
-    vector <Student> Winners;
-    vector <Student> Losers;
-
-    Group(Students, s, Losers, Winners);
-    long int w = Winners.size();
-    long int l = Losers.size();
-
-    start = std::chrono::steady_clock::now();
-    if (final)
-    {
-        Print(Winners, w, outputAverage, "Winners");
-        Print(Losers, l, outputAverage, "Losers");
-    }
-    else
-    { 
-        Print(Winners, w, outputMedian, "Winners");
-        Print(Losers, l, outputMedian, "Losers");
-    }
-    cout << "Time taken to output data: " << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << " ms" << endl;
-
-    Winners.clear();
-    Losers.clear();
+    
     WINPAUSE;
 }
