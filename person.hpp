4e886c134a585abb4e42199d5f9730d94d6da717
aistestonciute <72549610+aistestonciute@users.noreply.github.com>
2021-05-12 09:45:41 +0300

    Update person.hpp

--- person.hpp
+++ person.hpp
@@ -2,18 +2,16 @@
 #define PERSON_HPP
 
 #include <string>
-#include <vector>
 using std::string;
 
 class Person{
 protected:
     string name, lastName;
-
 public:
-    virtual ~Person() {};
     string getName() const {return name;}
     string getLastName() const {return lastName;}
     void setName(string _name) {name = _name;}
     void setLastName(string _lastName) {lastName = _lastName;}
+    virtual ~Person() = 0;
 };
 #endif
